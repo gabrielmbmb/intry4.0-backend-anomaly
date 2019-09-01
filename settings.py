65a26bb841c1ace19070c59ba16078fbd8c32317
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2019-09-01 14:26:41 +0200

    Update settings

--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
-APP_NAME = os.environ.get('APP_NAME')
-APP_DESC = os.environ.get('APP_DESC')
-APP_VERSION = os.environ.get('APP_VERSION')
-MODELS_ROUTE = os.environ.get('MODELS_ROUTE')
-MODELS_ROUTE_JSON = os.environ.get('MODELS_ROUTE_JSON')
-BROKER_URL = os.environ.get('BROKER_URL')
+APP_NAME = os.environ.get('APP_NAME') or 'PLATINUM - Blackbox Anomaly Detection'
+APP_DESC = os.environ.get('APP_DESC') or 'A simple API to call the Blackbox Anomaly Detection model.'
+APP_VERSION = os.environ.get('APP_VERSION') or '1.0'
+MODELS_ROUTE = os.environ.get('MODELS_ROUTE') or './models'
+MODELS_ROUTE_JSON = os.environ.get('MODELS_ROUTE_JSON') or os.path.join(MODELS_ROUTE, 'models.json')
+BROKER_URL = os.environ.get('BROKER_URL') or 'amqp://localhost'
