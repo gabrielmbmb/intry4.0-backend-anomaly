89cefbc66e7d12c2e8204267b6c286404f1f5a94
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2019-09-02 10:43:59 +0200

    Add API variables

--- settings.py
+++ settings.py
@@ -8,6 +8,9 @@ if os.path.exists(dotenv_path):
 APP_NAME = os.environ.get('APP_NAME') or 'PLATINUM - Blackbox Anomaly Detection'
 APP_DESC = os.environ.get('APP_DESC') or 'A simple API to call the Blackbox Anomaly Detection model.'
 APP_VERSION = os.environ.get('APP_VERSION') or '1.0'
+APP_HOST = os.environ.get('APP_HOST') or 'localhost'
+APP_PORT = os.environ.get('APP_PORT') or '5678'
+API_ANOMALY_ENDPOINT = os.environ.get('API_ANOMALY_ENDPOINT') or 'api/v1/anomaly'
 MODELS_ROUTE = os.environ.get('MODELS_ROUTE') or './models'
 MODELS_ROUTE_JSON = os.environ.get('MODELS_ROUTE_JSON') or os.path.join(MODELS_ROUTE, 'models.json')
 BROKER_URL = os.environ.get('BROKER_URL') or 'amqp://localhost'
