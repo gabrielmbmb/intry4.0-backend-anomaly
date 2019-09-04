afa4c3b69736f55d0f2481c5886ce40bc624056f
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2019-09-04 13:23:56 +0200

    Replace os.environ.get() for os.getenv()

--- settings.py
+++ settings.py
@@ -5,13 +5,14 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-APP_NAME = os.environ.get('APP_NAME') or 'PLATINUM - Blackbox Anomaly Detection'
-APP_DESC = os.environ.get('APP_DESC') or 'A simple API to call the Blackbox Anomaly Detection model.'
-APP_VERSION = os.environ.get('APP_VERSION') or '1.0'
-APP_HOST = os.environ.get('APP_HOST') or 'localhost'
-APP_PORT = os.environ.get('APP_PORT') or '5678'
-API_ANOMALY_ENDPOINT = os.environ.get('API_ANOMALY_ENDPOINT') or 'api/v1/anomaly'
-MODELS_ROUTE = os.environ.get('MODELS_ROUTE') or './models'
-MODELS_ROUTE_JSON = os.environ.get('MODELS_ROUTE_JSON') or os.path.join(MODELS_ROUTE, 'models.json')
-CELERY_BROKER_URL = os.environ.get('BROKER_URL') or 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND') or 'redis://localhost:6379/0'
+APP_DEBUG = 'True' == os.getenv('APP_DEBUG', 'False')
+APP_NAME = os.getenv('APP_NAME', 'PLATINUM - Blackbox Anomaly Detection')
+APP_DESC = os.getenv('APP_DESC', 'A simple API to call the Blackbox Anomaly Detection model.')
+APP_VERSION = os.getenv('APP_VERSION', '1.0')
+APP_HOST = os.getenv('APP_HOST', 'localhost')
+APP_PORT = os.getenv('APP_PORT', '5678')
+API_ANOMALY_ENDPOINT = os.getenv('API_ANOMALY_ENDPOINT', 'api/v1/anomaly')
+MODELS_ROUTE = os.getenv('MODELS_ROUTE', './models')
+MODELS_ROUTE_JSON = os.getenv('MODELS_ROUTE_JSON', os.path.join(MODELS_ROUTE, 'models.json'))
+CELERY_BROKER_URL = os.getenv('BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
