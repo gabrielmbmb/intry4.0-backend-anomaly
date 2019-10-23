6919603754b37faa3359fbf7f65b97190fcae6ba
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2019-10-23 17:46:21 +0200

    Update settings

--- blackbox/settings.py
+++ blackbox/settings.py
@@ -6,12 +6,12 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 APP_DEBUG = 'True' == os.getenv('APP_DEBUG', 'False')
-APP_NAME = os.getenv('APP_NAME', 'PLATINUM - Blackbox Anomaly Detection')
-APP_DESC = os.getenv('APP_DESC', 'A simple API to call the Blackbox Anomaly Detection model.')
-APP_HOST = os.getenv('APP_HOST', 'localhost')
+APP_NAME = 'PLATINUM - Blackbox Anomaly Detection'
+APP_DESC = 'A simple API to call the Blackbox Anomaly Detection model.'
+APP_HOST = os.getenv('APP_HOST', '0.0.0.0')
 APP_PORT = os.getenv('APP_PORT', '5678')
 API_ANOMALY_ENDPOINT = os.getenv('API_ANOMALY_ENDPOINT', 'api/v1/anomaly')
-MODELS_ROUTE = os.getenv('MODELS_ROUTE', os.path.abspath('./models'))
+MODELS_ROUTE = os.getenv('MODELS_ROUTE', os.path.expanduser('~/blackbox/models'))
 MODELS_ROUTE_JSON = os.getenv('MODELS_ROUTE_JSON', os.path.join(MODELS_ROUTE, 'models.json'))
 MODELS_ROUTE_TRASH = os.getenv('MODELS_ROUTE_TRASH', os.path.join(MODELS_ROUTE, 'trash'))
 CELERY_BROKER_URL = os.getenv('BROKER_URL', 'redis://localhost:6379/0')
