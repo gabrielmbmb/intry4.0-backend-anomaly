924335beb21a78c4716b90d298cd68b2a5456a10
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2019-11-04 17:00:38 +0100

    Create utils package

--- blackbox/utils/orion.py
+++ blackbox/utils/orion.py
@@ -1,4 +1,7 @@
+import os
 import requests
+from blackbox.
+from blackbox.settings import MODELS_ROUTE
 from typing import Union
 from blackbox.settings import FIWARE_SERVICE, FIWARE_SERVICEPATH, ORION_CONTEXT_BROKER
 
@@ -18,11 +21,16 @@ def update_entity_attrs(entity_id, attrs) -> Union[None, requests.Response]:
         return None
 
     url = '{}/v2/entities/{}/attrs'.format(ORION_CONTEXT_BROKER, entity_id)
-    print(url)
-    response = requests.post(url, json=attrs, headers={
-        'fiware-servicepath': FIWARE_SERVICEPATH,
-        'fiware-service': FIWARE_SERVICE,
-        'Content-Type': 'application/json'
-    })
+
+    try:
+        response = requests.post(url, json=attrs, headers={
+            'fiware-servicepath': FIWARE_SERVICEPATH,
+            'fiware-service': FIWARE_SERVICE,
+            'Content-Type': 'application/json'
+        })
+    except requests.exceptions.RequestException:
+        predictions_path = MODELS_ROUTE + '/predictions.json'
+        if os.path.exists(predictions_path):
+            print("Could not connect with Orion Context Broker. Saving prediction in {}".format('file'))
 
     return response
