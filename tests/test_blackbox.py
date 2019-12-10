32fa7aba45af2e3a514879107b167b64de1e1344
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2019-12-10 10:14:40 +0100

    Update Blackbox test

--- tests/test_blackbox.py
+++ tests/test_blackbox.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
 from blackbox.blackbox import BlackBoxAnomalyDetection, NotAnomalyModelClass
-from blackbox.models import AnomalyPCAMahalanobis, AnomalyAutoencoder, AnomalyKMeans, AnomalyIsolationForest, \
-    AnomalyGaussianDistribution, AnomalyOneClassSVM
+from blackbox.models import (
+    AnomalyPCAMahalanobis,
+    AnomalyAutoencoder,
+    AnomalyKMeans,
+    AnomalyIsolationForest,
+    AnomalyGaussianDistribution,
+    AnomalyOneClassSVM,
+)
 from blackbox.utils.csv import CSVReader
 from keras.regularizers import l1
 
@@ -12,56 +18,63 @@ class TestBlackBoxAnomalyDetection(TestCase):
     def setUp(self) -> None:
         self.model = BlackBoxAnomalyDetection(verbose=True)
         self.model.add_model(AnomalyPCAMahalanobis(verbose=True))
-        self.model.add_model(AnomalyAutoencoder(verbose=True,
-                                                hidden_neurons=[4, 2, 2, 4]))
+        self.model.add_model(
+            AnomalyAutoencoder(verbose=True, hidden_neurons=[4, 2, 2, 4])
+        )
         self.model.add_model(AnomalyKMeans(verbose=True))
         self.model.add_model(AnomalyOneClassSVM(verbose=True))
         self.model.add_model(AnomalyIsolationForest(verbose=True))
         self.model.add_model(AnomalyGaussianDistribution(verbose=True))
 
         self.model_name = BlackBoxAnomalyDetection(verbose=True)
+        self.model_name.add_model(AnomalyPCAMahalanobis(verbose=True), "PCAMahalanobis")
         self.model_name.add_model(
-            AnomalyPCAMahalanobis(verbose=True), 'PCAMahalanobis')
-        self.model_name.add_model(AnomalyAutoencoder(kernel_regularizer=l1(0.0),
-                                                     verbose=True,
-                                                     hidden_neurons=[4, 2, 2, 4]),
-                                  'Autoencoder')
-        self.model_name.add_model(AnomalyKMeans(_n_clusters=20,
-                                                verbose=True),
-                                  'KMeans')
-        self.model_name.add_model(AnomalyOneClassSVM(verbose=True),
-                                  'OneClassSVM')
-        self.model_name.add_model(AnomalyIsolationForest(verbose=True),
-                                  'IsolationForest')
-        self.model_name.add_model(AnomalyGaussianDistribution(verbose=True),
-                                  'GaussianDistribution')
+            AnomalyAutoencoder(
+                kernel_regularizer=l1(0.0), verbose=True, hidden_neurons=[4, 2, 2, 4]
+            ),
+            "Autoencoder",
+        )
+        self.model_name.add_model(AnomalyKMeans(_n_clusters=20, verbose=True), "KMeans")
+        self.model_name.add_model(AnomalyOneClassSVM(verbose=True), "OneClassSVM")
+        self.model_name.add_model(
+            AnomalyIsolationForest(verbose=True), "IsolationForest"
+        )
+        self.model_name.add_model(
+            AnomalyGaussianDistribution(verbose=True), "GaussianDistribution"
+        )
 
     def test_add_model(self):
         """Tests that models are correctly added to the Blackbox"""
         self.assertIsInstance(
-            self.model.models['AnomalyPCAMahalanobis'], AnomalyPCAMahalanobis)
+            self.model.models["AnomalyPCAMahalanobis"], AnomalyPCAMahalanobis
+        )
         self.assertIsInstance(
-            self.model.models['AnomalyAutoencoder'], AnomalyAutoencoder)
+            self.model.models["AnomalyAutoencoder"], AnomalyAutoencoder
+        )
+        self.assertIsInstance(self.model.models["AnomalyKMeans"], AnomalyKMeans)
         self.assertIsInstance(
-            self.model.models['AnomalyKMeans'], AnomalyKMeans)
+            self.model.models["AnomalyOneClassSVM"], AnomalyOneClassSVM
+        )
         self.assertIsInstance(
-            self.model.models['AnomalyOneClassSVM'], AnomalyOneClassSVM)
+            self.model.models["AnomalyIsolationForest"], AnomalyIsolationForest
+        )
         self.assertIsInstance(
-            self.model.models['AnomalyIsolationForest'], AnomalyIsolationForest)
-        self.assertIsInstance(
-            self.model.models['AnomalyGaussianDistribution'], AnomalyGaussianDistribution)
+            self.model.models["AnomalyGaussianDistribution"],
+            AnomalyGaussianDistribution,
+        )
 
         self.assertIsInstance(
-            self.model_name.models['PCAMahalanobis'], AnomalyPCAMahalanobis)
+            self.model_name.models["PCAMahalanobis"], AnomalyPCAMahalanobis
+        )
+        self.assertIsInstance(self.model_name.models["Autoencoder"], AnomalyAutoencoder)
+        self.assertIsInstance(self.model_name.models["KMeans"], AnomalyKMeans)
+        self.assertIsInstance(self.model_name.models["OneClassSVM"], AnomalyOneClassSVM)
         self.assertIsInstance(
-            self.model_name.models['Autoencoder'], AnomalyAutoencoder)
-        self.assertIsInstance(self.model_name.models['KMeans'], AnomalyKMeans)
+            self.model_name.models["IsolationForest"], AnomalyIsolationForest
+        )
         self.assertIsInstance(
-            self.model_name.models['OneClassSVM'], AnomalyOneClassSVM)
-        self.assertIsInstance(
-            self.model_name.models['IsolationForest'], AnomalyIsolationForest)
-        self.assertIsInstance(
-            self.model_name.models['GaussianDistribution'], AnomalyGaussianDistribution)
+            self.model_name.models["GaussianDistribution"], AnomalyGaussianDistribution
+        )
 
     def test_add_model_no_class_anomaly(self):
         """Tests that an error is raised if an instance of a class that doesn't inherit from AnomalyClassModel
@@ -71,11 +84,14 @@ class TestBlackBoxAnomalyDetection(TestCase):
 
     def test_train_save_load_predict_model(self):
         """Tests training, saving, loading and predicting  a Blackbox"""
-        reader = CSVReader('./tests/train_data.csv')  # read csv
+        reader = CSVReader("./test_csv/no_anomaly_data.csv")  # read csv
         df = reader.get_df()
 
+        reader2 = CSVReader("./test_csv/all_data_anomalies_included.csv")
+        df2 = reader2.get_df()
+
         def cb_function(progress, message):
-            print(message, 'Progress: ', progress)
+            print(message, "Progress: ", progress)
 
         self.model.train_models(df, cb_function)  # train model
 
@@ -88,4 +104,5 @@ class TestBlackBoxAnomalyDetection(TestCase):
         self.model.load_models()
 
         # predict
-        results = self.model.flag_anomaly([[0.72, 0.84, 0.22, 0.66]])
+        results = self.model.flag_anomaly(df2)
+        print(results)
