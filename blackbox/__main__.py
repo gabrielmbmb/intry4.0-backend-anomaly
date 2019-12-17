6d84caeb6cb3999f821a7a507e0f449e330544a5
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2019-12-17 16:11:11 +0100

    Add Gunicorn WSGI production server

--- blackbox/__main__.py
+++ blackbox/__main__.py
@@ -1,7 +1,35 @@
-from blackbox.api.api import run_api
+from gunicorn.app.base import BaseApplication
+from blackbox.api.api import app
+from blackbox import settings
+
+
+class GunicornApp(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
 
 def main():
-    run_api()
-    
+    options = {
+        "bind": "{}:{}".format(settings.APP_HOST, settings.APP_PORT),
+        "workers": 1,
+        "threads": 12,
+    }
+    GunicornApp(app, options).run()
+
+
 if __name__ == "__main__":
     main()
--- blackbox/api/api.py
+++ blackbox/api/api.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restplus import Api
 from blackbox import settings
 from blackbox import version
-from blackbox.utils.orion import check_orion_connection
 from blackbox.api.anomaly.namespace import anomaly_ns
 
 # Create Flask App
@@ -19,24 +18,3 @@ api = Api(
 
 # Add namespaces to the API
 api.add_namespace(anomaly_ns)
-
-
-def run_api():
-    """Runs the API with the configuration inside the config file"""
-    if not check_orion_connection():
-        print("Unable to connect to Orion Context Broker...")
-        print("Blackbox will continue its execution anyway...")
-    else:
-        print("Orion Context Broker is up")
-
-    if settings.API_SSL:
-        app.run(
-            host=settings.APP_HOST,
-            port=settings.APP_PORT,
-            debug=settings.APP_DEBUG,
-            ssl_context="adhoc",
-        )
-    else:
-        app.run(
-            host=settings.APP_HOST, port=settings.APP_PORT, debug=settings.APP_DEBUG
-        )
