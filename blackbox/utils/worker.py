6af4a1499cfd192f9566e7e0e2ef53b27743b1a5
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2020-03-29 11:38:04 +0200

    Remove unused modules

--- blackbox/utils/worker.py
+++
@@ -1,4 +0,0 @@
-from blackbox.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
-from celery import Celery
-
-celery_app = Celery("tasks", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
